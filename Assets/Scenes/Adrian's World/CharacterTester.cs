45ea718b6dfe5c5db7b46b96fd63c2095f8d2e49
Adrian Gilbert Ratuita <adrianratuita@gmail.com>
2018-10-11 05:13:57 -0500

    Timer Script
    Building up initial code for Timer script

--- Assets/Scenes/Adrian's World/CharacterTester.cs
+++ Assets/Scenes/Adrian's World/CharacterTester.cs
@@ -56,7 +56,6 @@ public class CharacterTester : MonoBehaviour
         InputHandler();
         AdjustCamera();
 
-
 	}
 
     void InputHandler()
---
+++ Assets/Scenes/Adrian's World/TimerScript.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour
+{
+
+    event EventHandler<CustomTimerEvent>     CountdownDone;
+
+    [SerializeField]
+    float timerCountdown;
+
+    //
+    bool isCounting = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+
+
+	}
+
+    private void FixedUpdate()
+    {
+        if (isCounting)
+        {
+
+            timerCountdown -= Time.fixedDeltaTime;
+
+        }
+        if (timerCountdown < 0)
+        {
+
+            isCounting = false;
+            //OnCountdownDone(new CustomTimerEvent());
+
+        }
+
+    }
+
+    public void StartTimer(float countdownTo)
+    {
+
+        isCounting = true;
+        timerCountdown = countdownTo;
+
+    }
+
+    public void StopTimer()
+    {
+
+        isCounting = false;
+
+    }
+
+    public void TimerLoseTime(float timeLost)
+    {
+
+        timerCountdown -= timeLost;
+
+    }
+
+    public void OnCountdownDone(CustomTimerEvent e)
+    {
+
+        EventHandler<CustomTimerEvent> handler = CountdownDone;
+        if (handler != null) { handler(this, e); }
+
+    }
+
+}
+
+public class CustomTimerEvent : EventArgs
+{
+
+    public CustomTimerEvent(string s)
+    {
+        message = s;
+    }
+    private string message;
+
+    public string Message
+    {
+        get { return message; }
+        set { message = value; }
+    }
+
+}
