48e0cb5a33f701b3b9c3015849a64b9f9a7422fc
Adrian Gilbert Ratuita <adrianratuita@gmail.com>
2018-10-20 06:52:48 -0500

    Timer Finished
    Added ITimer Interface
    Added Regular Timer
    Added Dynamic Timer

---
+++ Assets/Scenes/Adrian's World/DynamicTimer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class DynamicTimer : RegularTimer
+{
+    public DynamicTimer(TimerManager manager, float start, float end) : base(manager, start, end)
+    {
+    }
+
+    public DynamicTimer(TimerManager manager, float start, float end, RunTimer TimerDelegate) : base(manager, start, end, TimerDelegate)
+    {
+    }
+
+    public DynamicTimer(TimerManager manager, float start, float end, RunTimer TimerDelegate, GameObject gameObject) : base(manager, start, end, TimerDelegate, gameObject)
+    {
+    }
+
+    // Add time to the counter
+    public void AddTime(float moreTime)
+    {
+
+        startTime -= moreTime;
+
+    }
+
+    // Subtract time to the counter
+    public void SubtractTime(float lessTime)
+    {
+
+        startTime += lessTime;
+
+    }
+
+    // Add time to the passed timer's counter
+    public static void AddTime(DynamicTimer timerToAddTimeTo, float moreTime)
+    {
+
+        timerToAddTimeTo.AddTime(moreTime);
+
+    }
+
+    // Subtract time to the passed timer's counter
+    public static void SubtractTime(DynamicTimer timerToSubtractTimeTo, float lessTime)
+    {
+
+        timerToSubtractTimeTo.SubtractTime(lessTime);
+
+    }
+
+}
