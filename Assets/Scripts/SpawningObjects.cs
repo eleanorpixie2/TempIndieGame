2c01a70b7bdcabb7bdf1cd20f4ee6bc92b1b2e29
AdanStudent <adan.benitez@loop.colum.edu>
2018-10-23 00:37:58 -0500

    Player Spawn
    Running into issues trying to spawn player and only change its' tag and not the prefab as well.

--- Assets/Scripts/SpawningObjects.cs
+++ Assets/Scripts/SpawningObjects.cs
@@ -13,18 +13,57 @@ public class SpawningObjects : MonoBehaviour {
     [SerializeField]
     int NumOfItemsToSpawn = 100;
 
+    public List<GameObject> Statues = new List<GameObject>();
+
+    public List<GameObject> Players = new List<GameObject>();
+
     private BoxCollider SpawnArea;
+
+    private Material HiderMaterial;
 
 	// Use this for initialization
 	void Start ()
     {
-        //SceneItems = new List<GameObject>(); //List of Prefabs used to generate objects in the scene
+        //Retreiving a Reference of the BoxCollider for its bounds
+        SpawnArea = GetComponent<BoxCollider>();
+
+        //finds a Material to Tag the Players for debugging
+        DebugPlayers();
+
+        //Does the Actual Spawning of the Objects in SceneItems
         SpawnRandomObjects();
 
-        SpawnArea = GetComponent<BoxCollider>();
-        Debug.Log(SpawnArea.bounds);
+        //Places the Players Randomly in the scene
+        SpawnHiders();
     }
 
+    private void DebugPlayers()
+    {
+        foreach (var mat in FindObjectsOfType<Material>())
+        {
+            if (mat.name == "HiderMat")
+            {
+                HiderMaterial = mat;
+                break;
+            }
+        }
+    }
+
+    private void SpawnHiders()
+    {
+        //for i number of players
+        for (int i = 0; i < 3; i++)
+        {
+            int numOfPlayers = Random.Range(1, Statues.Count - 1);
+            //Statues[numOfPlayers].gameObject
+            Statues[numOfPlayers].gameObject.tag = string.Format("Hider{0}", i);
+            Statues[numOfPlayers].gameObject.GetComponent<Renderer>().material.color = Color.green;
+
+        }
+    }
+
+
+    //used to keep a reference of how large of a range the Random will be
     private int MaxRange = 10000; //10 thousand
     private void SpawnRandomObjects()
     {
@@ -37,37 +76,85 @@ public class SpawningObjects : MonoBehaviour {
             while (!isSpawned)
             {
                 float rand = Random.Range(0, MaxRange); //random number between 0 and the MaxRange
-                int ratio = MaxRange / 3; //Figuring out the Ratio to be checked against 33% for this one
+                int ratio = MaxRange / SceneItems.Count; //Figuring out the Ratio to be checked against 33% for this one
 
-                if (rand < ratio)
-                {
-                    spawnedItem = SceneItems[0];
-                    isSpawned = true;
-                }
-                else if (rand < ratio * 2)
-                {
-                    spawnedItem = SceneItems[1];
-                    isSpawned = true;
-                }
-                else
-                {
-                    spawnedItem = SceneItems[2];
-                    isSpawned = true;
-                }
+                isSpawned = GetRandomObject(out spawnedItem, rand, ratio);
 
+                //Stores value of spawnedItem's location
                 location = SwapXYValuesFor3D();
 
-                Instantiate(spawnedItem, location, Quaternion.identity, this.transform);
+                //checks if spawned was true before Creating object in the scene
+                if (isSpawned)
+                {
+                    if (spawnedItem.tag == "HiderPlaceholder")
+                    {
+                        Statues.Add(spawnedItem);
+                    }
+
+                    Instantiate(spawnedItem, location, Quaternion.identity, this.transform);
+                }
             }
         }
     }
 
+    private bool GetRandomObject(out GameObject spawnedItem, float rand, int ratio)
+    {
+        if (rand < ratio)
+        {
+            spawnedItem = SceneItems[0];
+            return true;
+        }
+        else if (rand < ratio * 2)
+        {
+            spawnedItem = SceneItems[1];
+            return true;
+        }
+        else if (rand < ratio * 3)
+        {
+            spawnedItem = SceneItems[2];
+            return true;
+        }
+        else if (rand < ratio * 4)
+        {
+            spawnedItem = SceneItems[3];
+            return true;
+        }
+        else if (rand < ratio * 5)
+        {
+            spawnedItem = SceneItems[4];
+            return true;
+        }
+        else if (rand < ratio * 6)
+        {
+            spawnedItem = SceneItems[5];
+            return true;
+        }
+
+        spawnedItem = null;
+        return false;
+
+    }
+
     private Vector3 SwapXYValuesFor3D()
     {
+        float size;
+
+        if (SpawnArea.size.x <= SpawnArea.size.z)
+        {
+            size = SpawnArea.size.x;
+        }
+        else
+        {
+            size = SpawnArea.size.z;
+        }
+
         //since the UnitCircle is a Radius of 1 need to halve the bounds
-        Vector3 location = Random.insideUnitCircle * (300 / 2);
+        Vector3 location = Random.insideUnitCircle * (size / 2);
+        
+        //swap z and y points because spawning on a flat plane
         location.z = location.y;
-        location.y = 0;
+        location.y = 0.5f;
+
         return location;
     }
 
